5977e98410065c2c57dfe9b75cbe2e0198c24e83
Daniel A-Sereboo <dsereboo@gmail.com>
2021-06-16 21:05:37 +0000

    Home page component completed with creation of floating button component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import {Container,Row, Col} from "react-bootstrap"
+import FloatButton from './components/FloatButton';
 
 function App() {
   return (
-    <h1>Hello</h1>
+    <Container>
+      <Row className="no-flex">
+        <h1 className="text-center">Notes</h1>
+      </Row>
+      <Row>
+        <Col>
+          <p>Notes come here</p>
+        </Col>
+      </Row>
+      <FloatButton/>
+    </Container>
   );
 }
 
---
+++ src/components/FloatButton.jsx
@@ -0,0 +1,25 @@
+import React from "react"
+import {Button} from "react-bootstrap"
+
+const FloatButton=()=>{
+    return(
+        <Button
+            variant="primary"
+            size="lg"
+            className="btn-float"
+        >
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="36"
+                height="32"
+                fill="currentColor"
+                className="bi bi-plus custom"
+                viewBox="0 0 16 16"
+            >
+                <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+            </svg>
+        </Button>
+    )
+}
+
+export default FloatButton
